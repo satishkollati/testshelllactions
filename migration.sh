c23a56f0f770bc314dec4d78adb24378f816cbf3
satishkollati <101156460+satishkollati@users.noreply.github.com>
2023-09-13 12:09:52 +0530

    Update migration.sh

--- migration.sh
+++ migration.sh
@@ -7,7 +7,7 @@ WORKSPACE='codepipeline9'
 projectkey='BB2GHMIG'
 bitbucket_pass="ATBBk5MJVVd7eTPajnKFCC3zW5Vg3B3BCDE6"
 GIT_USERNAME="satishkollati"
-GIT_PASSWORD="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"
+GIT_PASSWORD="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"
 #*********** vars *************************************
 #read -sp "Enter your bitbucket pass: " bitbucket_pass
 #read -sp "Enter your git pass: " GIT_PASSWORD
@@ -37,7 +37,7 @@ cat repos.txt | while read REPO; do
     # Pushing mirror to github repository
     #git push --mirror git@github.com:$GIT_USERNAME/$REPO.git
     #git push --mirror https://github.com/$GIT_USERNAME/$REPO.git
-    git push https://satishkollati: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@github.com/satishkollati/$REPO.git
+    git push https://satishkollati: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@github.com/satishkollati/$REPO.git
     cd ..
     # Remote local repo
     rm -rf $REPO
